e9a9f62085bc447afa97404e00869397a2d25c81
B <b@bvlmnnj.com>
2021-07-03 18:34:55 -0400

    Update to use org mode for source emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,10 @@
 (setq-default indent-tabs-mode nil)
 
 (show-paren-mode 1)
-(setq show-paren-style 'expression)
+;(setq show-paren-style 'expression)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-x <") 'org-insert-structure-template)
 
 (desktop-save-mode 1)
 
@@ -19,9 +22,20 @@
   kept-old-versions 2
   version-control t)
 
-(icomplete-mode 1) ;Change what happens on C-x b
-(global-set-key (kbd "C-x C-b") 'bs-show)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive "^") ; Use (interactive) in Emacs 22 or older
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key [home] 'smart-beginning-of-line)
 
 ;; Bootsrap straight.el
 (setq straight-repository-branch "master")
@@ -42,10 +56,66 @@
 
 (straight-use-package 'use-package)
 
-(use-package magit
+(use-package auto-package-update
   :config
-  (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+  (setq auto-package-update-prompt-before-update t)
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe)
   )
+
+(defun echo/org-mode-setup ()
+    (org-indent-mode)
+    (visual-line-mode 1)
+    )
+  (use-package org
+    :hook (org-mode . echo/org-mode-setup)
+    :config
+    (setq org-ellipsis " ▾")
+    )
+
+;  (use-package org-bullets
+;        :after org
+;        :hook (org-mode . org-bullets-mode)
+;        :custom
+;        (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●"))
+;        )
+
+  (defun echo/org-mode-visual-fill ()
+        (setq visual-fill-column-width 100
+              visual-fill-column-center-text t)
+        (visual-fill-column-mode 1))
+  (use-package visual-fill-column
+        :hook (org-mode . echo/org-mode-visual-fill))
+
+(org-babel-do-load-languages
+    'org-babel-load-languages
+    '((emacs-lisp . t)
+      (python . t)))
+
+(defun echo/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/settings/.emacs.d/emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'echo/org-babel-tangle-config))
+)
+
+(use-package org-contrib
+      :after org
+      :config
+      (require 'org-tempo)
+      (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+      (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+      (add-to-list 'org-structure-template-alist '("py" . "src python"))
+      )
+
+(use-package magit
+    :config
+    (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+    )
 
 (use-package web-mode
   :config
@@ -56,48 +126,101 @@
   :mode "\\.yml\\'"
   )
 
-;(use-package vs-dark-theme)
-;(use-package dracula-theme)
-
-(use-package ivy
-  :disabled
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
   :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  )
-
-(use-package helm
-  :disabled
-  )
-
-(use-package bm
-  :bind
-  ("<C-left>" . bm-toggle)
-  ("<C-up>" . bm-previous)
-  ("<C-down>" . bm-next)
-  )
-
-(use-package powerline)
-(use-package moe-theme
-  :config
-  (setq moe-theme-highlight-buffer-id t)
-;  (moe-theme-set-color 'orange)
-  (moe-dark)
-  (powerline-moe-theme)
+  (setq which-key-idle-delay 0.5)
   )
 
 (use-package amx
-  :config
-  (amx-mode)
-  )
+      :config
+      (amx-mode)
+      )
 
 (use-package projectile
-  :disabled
-  :config
-  (projectile-mode +1)
-  :bind
-  ("C-p" . projectile-command-map)
+      :diminish projectile-mode
+      :config
+      (projectile-mode)
+      :custom
+      ((projectile-completion-system 'ivy))
+      :bind
+      ("C-p" . projectile-command-map)
+      :init
+      (when (file-directory-p "~/projects")
+        (setq projectile-project-search-path '("~/projects")))
+      )
+
+(use-package ivy
+:diminish
+:bind (("C-s" . swiper)
+       :map ivy-minibuffer-map
+       ("TAB" . ivy-alt-done)
+       ("C-j" . ivy-next-line)
+       ("C-k" . ivy-previous-line)
+       :map ivy-switch-buffer-map
+       ("C-k" . ivy-previous-line)
+       ("C-l" . ivy-done)
+       ("C-d" . ivy-switch-buffer-kill)
+       :map ivy-reverse-i-search-map
+       ("C-k" . ivy-previous-line)
+       ("C-d" . ivy-reverse-i-search-kill))
+:config
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-wrap t)
+)
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  :after
+  (ivy)
+    )
+
+(use-package hydra)
+
+(use-package ivy-hydra
+  :after
+  (ivy hydra)
   )
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(use-package counsel
+      :bind (
+             ("M-x" . counsel-M-x)
+             ("C-x b" . counsel-ibuffer)
+             ("C-x C-b" . counsel-ibuffer)
+             ("C-x C-f" . counsel-find-file)
+             :map minibuffer-local-map
+             ("C-r" . 'counsel-minibuffer-history)
+             )
+      )
+
+(use-package bm
+      :bind
+      ("<C-left>" . bm-toggle)
+      ("<C-up>" . bm-previous)
+      ("<C-down>" . bm-next)
+      )
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-gruvbox)
+  )
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  )
+
+(setq visible-bell t)
+
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook)
+              )
+  (add-hook mode (lambda () (display-line-numbers-mode 0)))
+  )
+
+
