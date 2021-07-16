d0824b522b0eea53e5a52474ae2a1fcb813fc299
B <b@bvlmnnj.com>
2021-07-16 14:48:04 -0400

    More updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -230,11 +230,6 @@
   (setq switch-window-shortcut-appearance 'asciiart)
   )
 
-(use-package golden-ratio
-  :config
-  (golden-radio-mode 1)
-  )
-
 (use-package doom-themes
   :config
   ;(load-theme 'doom-gruvbox)
@@ -255,3 +250,17 @@
               )
   (add-hook mode (lambda () (display-line-numbers-mode 0)))
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b7e460a67bcb6cac0a6aadfdc99bdf8bbfca1393da535d4e8945df0648fa95fb" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
