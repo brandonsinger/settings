c2a016300b196f2c74bea7721721cd2bf2207224
B <b@bvlmnnj.com>
2021-07-18 21:47:33 -0400

    .

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,8 @@
 
 (setq-default tab-width 4)
 
+(column-number-mode)
+
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "C-x <") 'org-insert-structure-template)
 
@@ -169,6 +171,8 @@
   (counsel-projectile-mode)
   )
 
+
+
 (use-package ivy
 :diminish
 :bind (
@@ -229,6 +233,46 @@
   (setq switch-window-minibuffer-shortcut ?z)
   (setq switch-window-shortcut-appearance 'asciiart)
   )
+; maybe use winmode instead of switch-window?
+
+(use-package winner-mode
+  :ensure nil
+  :straight nil
+  :config
+  (winner-mode)
+  )
+
+(use-package buffer-move)
+
+(defhydra hydra-mywindow ()
+     "
+  ^Change Window^   ^Buffer Move^      ^Window^         ^Resize Window^
+  -------------------------------------------
+      ↑     	        C-↑             Split _v_ertical    _<prior>_ Enlarge Horizontally
+      ↓     	        C-↓             Split _h_orizontal  _<next>_ Shrink Horizontally
+      ←     	        C-←             _k_ill              _<deletechar>_ Shrink Vertically
+      →               C-→             _u_ndo
+  _SPC_ cancel
+  "
+     ("<up>" windmove-up)
+     ("<down>" windmove-down)
+     ("<left>" windmove-left)
+     ("<right>" windmove-right)
+     ("C-<up>" buf-move-up)
+     ("C-<down>" buf-move-down)
+     ("C-<left>" buf-move-left)
+     ("C-<right>" buf-move-right)
+     ("v" split-window-right)
+     ("h" split-window-below)
+     ("k" delete-window)
+     ("u" winner-undo)
+     ("<prior>" enlarge-window-horizontally)
+     ("<next>" shrink-window-horizontally)
+     ("<deletechar>" shrink-window)
+     ("SPC" nil)
+     ("q" nil)
+     )
+(global-set-key (kbd "C-M-w") 'hydra-mywindow/body)
 
 (use-package doom-themes
   :config
