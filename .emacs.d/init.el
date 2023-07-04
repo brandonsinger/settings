85a6a8f1029e06c6f2e25fec5899acb34d4da40e
Echo <echo@dev.bvlmnnj.com>
2023-07-04 19:01:27 -0400

    Added some shell stuff and changed light theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -411,6 +411,157 @@
   (setq wttrin-default-cities '("48638"))
   )
 
+(setq tramp-default-method "ssh")
+
+(setenv "PAGER" "cat")
+
+(use-package eshell
+  :commands eshell
+  :init
+  (setq ;; eshell-directory-name (concat cpm-local-dir "eshell/")
+   ;; eshell-history-file-name (concat cpm-local-dir "eshell/history")
+   ;; eshell-aliases-file (concat cpm-local-dir "eshell/alias")
+   ;; eshell-last-dir-ring-file-name (concat cpm-local-dir "eshell/lastdir")
+   eshell-highlight-prompt nil
+   eshell-buffer-shorthand t
+   eshell-cmpl-ignore-case t
+   eshell-cmpl-cycle-completions t
+   eshell-destroy-buffer-when-process-dies t
+   eshell-history-size 10000
+   ;; auto truncate after 20k lines
+   eshell-buffer-maximum-lines 20000
+   eshell-hist-ignoredups t
+   eshell-error-if-no-glob t
+   eshell-glob-case-insensitive t
+   eshell-scroll-to-bottom-on-input 'all
+   eshell-scroll-to-bottom-on-output 'all
+   eshell-list-files-after-cd t
+   eshell-banner-message ""
+   )
+  ;; Visual commands
+  (setq eshell-visual-commands '("top" "less" "more" "top" "htop" "ssh" "tail"))
+  (setq eshell-visual-subcommands '(("git" "log" "diff" "show"))))
+
+(add-hook 'eshell-mode-hook (lambda ()
+                              (eshell/alias "e" "find-file $1")
+                              (eshell/alias "ff" "find-file $1")
+                              (eshell/alias "emacs" "find-file $1")
+                              (eshell/alias "ee" "find-file-other-window $1")
+
+                              (eshell/alias "gd" "magit-diff-unstaged")
+                              (eshell/alias "gds" "magit-diff-staged")
+                              (eshell/alias "d" "dired $1")
+
+                              (eshell/alias "ll" "ls -AlohG --color=always")))
+
+(defun eshell/clear ()
+  "Clear the eshell buffer."
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (eshell-send-input)))
+
+(with-eval-after-load 'eshell
+  (require 'dash)
+  (require 's)
+
+  (defmacro with-face (STR &rest PROPS)
+    "Return STR propertized with PROPS."
+    `(propertize ,STR 'face (list ,@PROPS)))
+
+  (defmacro esh-section (NAME ICON FORM &rest PROPS)
+    "Build eshell section NAME with ICON prepended to evaled FORM with PROPS."
+    `(setq ,NAME
+           (lambda () (when ,FORM
+                        (-> ,ICON
+                            (concat esh-section-delim ,FORM)
+                            (with-face ,@PROPS))))))
+
+  (defun esh-acc (acc x)
+    "Accumulator for evaluating and concatenating esh-sections."
+    (--if-let (funcall x)
+        (if (s-blank? acc)
+            it
+          (concat acc esh-sep it))
+      acc))
+
+  (defun esh-prompt-func ()
+    "Build `eshell-prompt-function'"
+    (concat esh-header
+            (-reduce-from 'esh-acc "" eshell-funcs)
+            "\n"
+            eshell-prompt-string))
+
+  (defun pwd-replace-home (pwd)
+    "Replace home in PWD with tilde (~) character."
+    (interactive)
+    (let* ((home (expand-file-name (getenv "HOME")))
+           (home-len (length home)))
+      (if (and
+           (>= (length pwd) home-len)
+           (equal home (substring pwd 0 home-len)))
+          (concat "~" (substring pwd home-len))
+        pwd)))
+
+
+  (defun pwd-shorten-dirs (pwd)
+    "Shorten all directory names in PWD except the last two."
+    (let ((p-lst (split-string pwd "/")))
+      (if (> (length p-lst) 2)
+          (concat
+           (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+                                      (substring elm 0 1)))
+                      (butlast p-lst 2)
+                      "/")
+           "/"
+           (mapconcat (lambda (elm) elm)
+                      (last p-lst 2)
+                      "/"))
+        pwd)))  ;; Otherwise, we just return the PWD
+
+  (esh-section esh-dir
+               ""  ;  (faicon folder)
+               (pwd-shorten-dirs (pwd-replace-home (eshell/pwd)))
+               '(:foreground "#268bd2" :underline t))
+
+  (esh-section esh-git
+               "\xe907"  ;  (git icon)
+               (with-eval-after-load 'magit
+                 (magit-get-current-branch))
+               '(:foreground "#b58900"))
+
+  (esh-section esh-python
+               "\xe928"  ;  (python icon)
+               (with-eval-after-load "virtualenvwrapper"
+                 venv-current-name))
+
+  (esh-section esh-clock
+               ""  ;  (clock icon)
+               (format-time-string "%H:%M" (current-time))
+               '(:foreground "forest green"))
+
+  ;; Separator between esh-sections
+  (setq esh-sep " | ")  ; or "  "
+
+  ;; Separator between an esh-section icon and form
+  (setq esh-section-delim " ")
+
+  ;; Eshell prompt header
+  (setq esh-header "\n ")  ; or "\n "
+
+  ;; Eshell prompt regexp and string. Unless you are varying the prompt by eg.
+  ;; your login, these can be the same.
+  (setq eshell-prompt-regexp "^>> ") ;; note the '^' to get regex working right
+  (setq eshell-prompt-string ">> ")
+
+  ;; Choose which eshell-funcs to enable
+  (setq eshell-funcs (list esh-dir esh-clock))
+
+  ;; Enable the new eshell prompt
+  (setq eshell-prompt-function 'esh-prompt-func))
+
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode))
+
 (use-package vertico
   :init
   (vertico-mode 1)
@@ -583,7 +734,7 @@
   :demand t
   :after (org)
   :init
-  (setq ef-themes-to-toggle '(ef-bio ef-day))
+  (setq ef-themes-to-toggle '(ef-bio ef-duo-light))
 
 
   (setq ef-themes-headings ; read the manual's entry or the doc string
