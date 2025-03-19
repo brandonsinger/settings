a590dbcb9c2fccec1af8a7ee4caf14848c5f562d
Brandon Singer <oshecho@gmail.com>
2025-03-19 00:09:50 -0400

    bug fixed

--- .emacs.d/lisp-init/init-path.el
+++ .emacs.d/lisp-init/init-path.el
@@ -6,7 +6,8 @@
 (setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
       url-history-file (expand-file-name "url/history" user-emacs-directory))
 
-;; bugged: (setq auto-save-file-name-transforms '((".*" (expand-file-name "auto-save-list" user-emacs-directory) t)))
+
+(setq auto-save-file-name-transforms `((".*" ,(expand-file-name "auto-save-list" user-emacs-directory) t)))
 
 ;; Set backup directory and backup settings
 (setq backup-directory-alist `(("." . ,(expand-file-name "backup" user-emacs-directory))))
