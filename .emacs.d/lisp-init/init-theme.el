503daf25dd30085445c7245fbbde3b4e3e7f6711
Brandon Singer <oshecho@gmail.com>
2025-04-05 21:59:28 -0400

    small theme update

--- .emacs.d/lisp-init/init-theme.el
+++ .emacs.d/lisp-init/init-theme.el
@@ -1,12 +1,11 @@
-;;; -*- lexical-binding: t; -*-
+;;; init-theme.el --- Theme stuff -*- lexical-binding: t; -*-
+;;; Commentary:
 ;;; Code:
 
 (use-package ef-themes
   :demand t
-  ;;:after (org)
   :init
-  (setq ef-themes-to-toggle '(ef-bio ef-duo-light))
-
+  (setq ef-themes-to-toggle '(ef-bio ef-duo-eagle))
 
   (setq ef-themes-headings ; read the manual's entry or the doc string
         '((0 . (variable-pitch light 1.9))
@@ -21,15 +20,6 @@
   ;; They are nil by default...
   (setq ef-themes-mixed-fonts t
         ef-themes-variable-pitch-ui t)
-
-  (setq ef-themes-region '(no-extend))
-
-  ;; ;; not working, must be doing something wrong
-  ;; (setq ef-bio-palette-overrides
-  ;;       '((cursor red)
-  ;;         (org-blocks green))
-  ;;       )
-
 
   ;; Disable all other themes to avoid awkward blending:
   (mapc #'disable-theme custom-enabled-themes)
