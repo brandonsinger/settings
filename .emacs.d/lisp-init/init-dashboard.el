3db4fe2160a92a79e966f9a1e543f85d80200866
Brandon Singer <oshecho@gmail.com>
2025-05-06 01:03:49 -0400

    added activities to dashboard

--- .emacs.d/lisp-init/init-dashboard.el
+++ .emacs.d/lisp-init/init-dashboard.el
@@ -2,33 +2,57 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun dashboard-insert-activities (list-size)
+  "Add the list of LIST-SIZE items from activities."
+  (dashboard-insert-section
+   "Activities:"
+   (map-keys activities-activities)
+   list-size
+   'activities
+   "a"
+   `(lambda (&rest _) (activities-resume (map-elt activities-activities ',el)))
+   (abbreviate-file-name el)))
+
+
 (use-package dashboard
   :after nerd-icons
   :config
+  (add-hook 'elpaca-after-init-hook #'dashboard-insert-startupify-lists)
+  (add-hook 'elpaca-after-init-hook #'dashboard-initialize)
+  (add-to-list 'dashboard-item-generators '(activities . dashboard-insert-activities))
+  ;;(add-to-list 'dashboard-heading-icons '(activities . "nf-oct-project"))
   (dashboard-setup-startup-hook)
-  (setopt dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-  (setopt dashboard-startup-banner 'logo)
-  (setopt dashboard-projects-backend 'projectile)
-  (setopt dashboard-items '((recents   . 5)
-                            (bookmarks . 5)
-                            (projects  . 5)
-                            (agenda    . 5)
-                            (registers . 5)))
-  (setopt dashboard-display-icons-p t)
-  (setopt dashboard-icon-type 'nerd-icons)
-  (setopt dashboard-set-file-icons t)
-  (setopt dashboard-startupify-list '(dashboard-insert-banner
-                                      dashboard-insert-newline
-                                      dashboard-insert-banner-title
-                                      dashboard-insert-newline
-                                      dashboard-insert-navigator
-                                      dashboard-insert-newline
-                                      dashboard-insert-init-info
-                                      dashboard-insert-items
-                                      dashboard-insert-newline
-                                      dashboard-insert-footer))
+  :custom
+  (dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+  (dashboard-startup-banner 'logo)
+  (dashboard-projects-backend 'projectile)
+  (dashboard-items '((activities . 5)
+                     (recents   . 5)
+                     (bookmarks . 5)
+                     (projects  . 5)
+                     (agenda    . 5)
+                     (registers . 5)))
+  (dashboard-display-icons-p t)
+  (dashboard-icon-type 'nerd-icons)
+  (dashboard-set-file-icons t)
+  (dashboard-startupify-list '(dashboard-insert-banner
+                               dashboard-insert-newline
+                               dashboard-insert-banner-title
+                               dashboard-insert-newline
+                               dashboard-insert-navigator
+                               dashboard-insert-newline
+                               dashboard-insert-init-info
+                               dashboard-insert-items
+                               dashboard-insert-newline
+                               dashboard-insert-footer))
   ;;(setopt dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
   )
+
+(use-package dashboard-ls
+  :disabled
+  :after dashboard
+  :config
+  (add-to-list 'dashboard-items '(ls-directories . 5)))
 
 (use-package votd
   :ensure(:host github :repo "kristjoc/votd")
