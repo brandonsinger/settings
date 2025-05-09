5f6e918b89e83120e31888af7825efe921f7d883
Brandon Singer <oshecho@gmail.com>
2025-05-09 13:37:18 -0400

    magit fix+cleanup

--- .emacs.d/lisp-init/init-tool-magit.el
+++ .emacs.d/lisp-init/init-tool-magit.el
@@ -3,26 +3,28 @@
 ;;;  use ~magit-list-repositories~ to get a status list of all projects
 ;;; Code:
 
+(use-package transient)
+
 (use-package magit
-  :commands (magit-status)
-  :config
-  (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
-  (setq magit-repository-directories '(("~/projects" . 1)))
-  (setq magit-repolist-columns
-        '(("Name"    25 magit-repolist-column-ident ())
-          ("Version" 25 magit-repolist-column-version ())
-          ("D"        1 magit-repolist-column-flag ())
-          ("B<U"      3 magit-repolist-column-unpulled-from-upstream
-           ((:right-align t)
-            (:help-echo "Upstream changes not in branch")))
-          ("B>U"      3 magit-repolist-column-unpushed-to-upstream
-           ((:right-align t)
-            (:help-echo "Local changes not in upstream")))
-          ("Path"    99 magit-repolist-column-path ())))
+  :bind
+  ("C-x g" . magit-status)
   :custom
+  (magit-repository-directories '(("~/projects" . 1)))
+  (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
   (magit-format-file-function #'magit-format-file-nerd-icons)
   (magit-save-repository-buffers 'dontask)
   (magit-log-margin '(t "%Y-%m-%d" magit-log-margin-width t 18))
+  (magit-repolist-columns
+   '(("Name"    25 magit-repolist-column-ident ())
+     ("Version" 25 magit-repolist-column-version ())
+     ("D"        1 magit-repolist-column-flag ())
+     ("B<U"      3 magit-repolist-column-unpulled-from-upstream
+      ((:right-align t)
+       (:help-echo "Upstream changes not in branch")))
+     ("B>U"      3 magit-repolist-column-unpushed-to-upstream
+      ((:right-align t)
+       (:help-echo "Local changes not in upstream")))
+     ("Path"    99 magit-repolist-column-path ())))
   )
 
 (use-package magit-todos
