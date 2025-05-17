c7ffcd68489575d9184cbba44f284a50f955b7c0
Brandon Singer <oshecho@gmail.com>
2025-05-16 23:50:51 -0400

    cleanup of treesit-auto

--- .emacs.d/lisp-init/init-treesitter.el
+++ .emacs.d/lisp-init/init-treesitter.el
@@ -5,25 +5,10 @@
 (use-package treesit-auto
   :hook (elpaca-after-init . global-treesit-auto-mode)
   :custom
-  (treesit-font-lock-level 5)
+  (treesit-font-lock-level 4)
   (treesit-auto-install 'prompt)
-  (treesit-language-source-alist
-   '(
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (php "https://github.com/tree-sitter/tree-sitter-php" "master" "php/src")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     (rust "https://github.com/tree-sitter/tree-sitter-rust")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
-     (bash "https://github.com/tree-sitter/tree-sitter-bash")
-     ))
-  (treesit-auto-langs '(javascript yaml json html css js elisp php rust toml))
   :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
 (use-package tree-sitter
