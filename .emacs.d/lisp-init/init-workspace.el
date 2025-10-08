b778b79fa77ffed722e10f45a3e820f3a8ee1852
Brandon Singer <oshecho@gmail.com>
2025-10-07 20:12:04 -0400

    changed what i use for 'workspaces'

--- .emacs.d/lisp-init/init-workspace.el
+++ .emacs.d/lisp-init/init-workspace.el
@@ -2,32 +2,40 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package easysession
+  :custom
+  (easysession-save-interval (* 10 60))
+  :init
+  (add-hook 'elpaca-after-init-hook #'easysession-load-including-geometry 98)
+  (add-hook 'elpaca-after-init-hook #'easysession-save-mode 99))
+
 (use-package tab-bar
   :ensure nil
   :bind
-  (("C-x t <left>" . tab-bar-switch-to-prev-tab)
-   ("C-x t <right>" . tab-bar-switch-to-next-tab)))
-
-;; TODO: use something else or customize it better
-(use-package activities
-  :init
-  (activities-mode)
-  (activities-tabs-mode)
-  ;; Prevent `edebug' default bindings from interfering.
-  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
-  :bind
-  (("C-x C-a C-n" . activities-new)
-   ("C-x C-a C-d" . activities-define)
-   ("C-x C-a C-a" . activities-resume)
-   ("C-x C-a C-s" . activities-suspend)
-   ("C-x C-a C-k" . activities-kill)
-   ("C-x C-a r" . activities-rename)
-   ("C-x C-a RET" . activities-switch)
-   ("C-x C-a b" . activities-switch-buffer)
-   ("C-x C-a g" . activities-revert)
-   ("C-x C-a l" . activities-list)
-   ("C-X C-a <DELETE>" . activities-discard)
+  (
+   ("C-x t <left>" . tab-bar-switch-to-prev-tab)
+   ("C-x t <right>" . tab-bar-switch-to-next-tab)
+   ("C-x t R" . tab-bar-rename-tab)
+   ("C-x t K" . tab-bar-close-tab)
+   ("C-x t N" . tab-bar-new-tab)
    ))
+
+;;; TODO: add Filter saved bookmark buffers stuff?
+(use-package bufferlo
+  :init
+  (setq bufferlo-bookmarks-save-at-emacs-exit 'all)
+  (setq bufferlo-bookmarks-load-at-emacs-startup 'all)
+  (bufferlo-mode)
+  :bind
+  (
+   ("C-x b" . bufferlo-switch-to-buffer)
+   ("C-x C-b" . bufferlo-switch-to-buffer)
+   ("C-x k" . bufferlo-remove) ;; or bufferlo-bury?
+   )
+  :config
+  (setopt bufferlo-bookmarks-auto-save-interval 120)
+  (setq bufferlo-bookmark-frame-save-on-delete 'if-current)
+  (setq bufferlo-bookmark-tab-save-on-close 'if-current))
 
 (provide 'init-workspace)
 ;;; init-workspace.el ends here
