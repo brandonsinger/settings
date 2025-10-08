d23e8db4b802ac733d935ff87fa952876b0c81ea
Brandon Singer <oshecho@gmail.com>
2025-10-07 21:19:07 -0400

    .

--- .emacs.d/lisp-init/init-workspace.el
+++ .emacs.d/lisp-init/init-workspace.el
@@ -3,11 +3,29 @@
 ;;; Code:
 
 (use-package easysession
+  :diminish easysession-save-mode
   :custom
   (easysession-save-interval (* 10 60))
   :init
   (add-hook 'elpaca-after-init-hook #'easysession-load-including-geometry 98)
   (add-hook 'elpaca-after-init-hook #'easysession-save-mode 99))
+
+(defun my/new-tab-with-scratch (tab-name)
+  "Create a new tab named TAB-NAME with *scratch* buffer."
+  (interactive "sTab name: ")
+  (tab-new)
+  (tab-rename tab-name)
+  (switch-to-buffer "*scratch*"))
+(defun my/projectile-new-tab-scratch ()
+  "Select a projectile project, open it in a new tab, and switch to *scratch* buffer."
+  (interactive)
+  (let* ((project (completing-read "Switch to project: "
+                                   projectile-known-projects))
+         (project-name (file-name-nondirectory (directory-file-name project))))
+    (tab-bar-new-tab)
+    (tab-bar-rename-tab project-name)
+    (let ((default-directory project))
+      (switch-to-buffer "*scratch*"))))
 
 (use-package tab-bar
   :ensure nil
@@ -17,7 +35,8 @@
    ("C-x t <right>" . tab-bar-switch-to-next-tab)
    ("C-x t R" . tab-bar-rename-tab)
    ("C-x t K" . tab-bar-close-tab)
-   ("C-x t N" . tab-bar-new-tab)
+   ("C-x t N" . my/new-tab-with-scratch)
+   ("C-x t P" . my/projectile-new-tab-scratch)
    ))
 
 ;;; TODO: add Filter saved bookmark buffers stuff?
