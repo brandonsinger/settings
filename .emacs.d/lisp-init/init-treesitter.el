5eda1347b5117ad63f7662929aaa8ced82b48f2f
Brandon Singer <oshecho@gmail.com>
2026-06-02 18:43:55 -0400

    treesit-fold changes

--- .emacs.d/lisp-init/init-treesitter.el
+++ .emacs.d/lisp-init/init-treesitter.el
@@ -37,7 +37,7 @@
              treesit-fold-line-comment-mode)
   :custom
   (treesit-fold-line-count-show t)
-  (treesit-fold-line-count-format " ▼")
+  (treesit-fold-indicators-mode " ▾")
   :config
   (set-face-attribute 'treesit-fold-replacement-face nil
                       :foreground "#808080"
@@ -51,6 +51,10 @@
 (add-hook 'rust-ts-mode-hook #'treesit-fold-mode)
 (add-hook 'go-ts-mode-hook #'treesit-fold-mode)
 (add-hook 'ruby-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'php-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'csharp-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'go-mod-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'lua-ts-mode-hook #'treesit-fold-mode)
 
 ;; Web and Frontend
 (add-hook 'js-ts-mode-hook #'treesit-fold-mode)
@@ -58,6 +62,8 @@
 (add-hook 'tsx-ts-mode-hook #'treesit-fold-mode)
 (add-hook 'css-ts-mode-hook #'treesit-fold-mode)
 (add-hook 'html-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'heex-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'xml-ts-mode-hook #'treesit-fold-mode)
 
 ;; Scripting and Infrastructure
 (add-hook 'bash-ts-mode-hook #'treesit-fold-mode)
@@ -67,6 +73,35 @@
 ;; Data and Configuration
 (add-hook 'json-ts-mode-hook #'treesit-fold-mode)
 (add-hook 'toml-ts-mode-hook #'treesit-fold-mode)
+
+;; Build Systems and Makefiles
+(add-hook 'makefile-ts-mode-hook #'treesit-fold-mode)
+
+;; Hardware Description and Shaders
+(add-hook 'verilog-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'vhdl-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'hlsl-ts-mode-hook #'treesit-fold-mode)
+
+;; Documentation and Diagrams
+(add-hook 'markdown-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'mermaid-ts-mode-hook #'treesit-fold-mode)
+
+;; Other
+(add-hook 'gdscript-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'clojure-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'caml-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'ocaml-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'erlang-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'elixir-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'scala-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'dart-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'haskell-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'julia-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'kotlin-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'gleam-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'noir-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'swift-ts-mode-hook #'treesit-fold-mode)
+(add-hook 'zig-ts-mode-hook #'treesit-fold-mode)
 
 ;; TODO: play with this more
 (use-package combobulate
